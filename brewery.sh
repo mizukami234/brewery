391e96ed9e5e06784dd567ab9daa94da173ec467
Shun MIZUKAMI <norainu234+dev@gmail.com>
2016-04-30 10:20:28 +0900

    add spectacle vagrant

--- brewery.sh
+++ brewery.sh
@@ -1,4 +1,5 @@
 brew cask install iterm2 || true
 brew cask install virtualbox || true
 brew cask install sublime-text || true
-
+brew cask install spectacle || true
+brew cask install vagrant || true
